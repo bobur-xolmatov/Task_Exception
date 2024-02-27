3248bcca338ead2261a76a188c836a18fde710dc
Bobur <Bobur@DESKTOP-SK029HJ>
2024-02-27 18:00:15 +0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿
+
+
+try
+{
+    Console.Write("Enter name : ");
+    string str = Console.ReadLine();
+
+    int result = LengthOfString(str);
+    Console.WriteLine(result);
+
+}
+
+catch (ArgumentException e)
+{
+    Console.WriteLine(e.Message);
+}
+
+int LengthOfString(string str)
+{
+    if (str.Length > 0)
+    {
+        return str.Length;
+    }
+    else
+    {
+        throw new ArgumentException("Str is null");
+    }
+}
+
+
+
